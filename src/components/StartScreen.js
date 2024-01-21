8c3e52f89f5e0ae56464077fac7434a8510ebbc9
Ahmad Hajjar <ahmed_hajjar@hotmail.com>
2024-01-21 12:35:34 +0100

    Styling start screen

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -1,4 +1,6 @@
 import React, {useState} from 'react';
+import '../styles/StartScreen.css';
+
 
 function StartScreen(props) {
     const [maxNumber, setMaxNumber] = useState(10);
@@ -59,63 +61,68 @@ function StartScreen(props) {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label>
-                Maximum Number:
-                <input type="number" value={maxNumber} onChange={handleNumberChange}/>
-            </label>
-            <br/>
-            <label>
-                Number of Equations:
-                <input type="number" value={numEquations} onChange={handleNumEquationsChange}/>
-            </label>
-            <br/>
-            <label>
-                Group Size:
-                <input type="number" value={groupSize} onChange={handleGroupSizeChange}/>
-            </label>
-            <br/>
-            <label>
-                Allow Negative Results:
-                <input
-                    type="checkbox"
-                    checked={allowNegativeResults}
-                    onChange={handleAllowNegativeResultsChange}
-                />
-            </label>
-            <br/>
-            <fieldset>
-                <legend>Operations</legend>
-                <label>
-                    <input type="checkbox" name="addition" checked={operations.addition}
-                           onChange={handleOperationChange}/>
-                    Addition
-                </label>
-                <label>
-                    <input type="checkbox" name="subtraction" checked={operations.subtraction}
-                           onChange={handleOperationChange}/>
-                    Subtraction
-                </label>
-                <label>
-                    <input type="checkbox" name="multiplication" checked={operations.multiplication}
-                           onChange={handleOperationChange}/>
-                    Multiplication
-                </label>
-                <label>
-                    <input type="checkbox" name="division" checked={operations.division}
-                           onChange={handleOperationChange}/>
-                    Division
-                </label>
-            </fieldset>
-            <br/>
-            <label>
-                Print Mode:
-                <input type="checkbox" checked={printMode} onChange={handlePrintModeChange}/>
-            </label>
-            <br/>
-            {error && <div style={{color: 'red'}}>{error}</div>}
-            <button type="submit">Start</button>
-        </form>
+        <div className="start-screen">
+            <h1>Kids Math App</h1>
+            <div className="form-container">
+                <form onSubmit={handleSubmit}>
+                    <label>
+                        Maximum Number
+                        <input type="number" value={maxNumber} onChange={handleNumberChange}/>
+                    </label>
+                    <br/>
+                    <label>
+                        Number of Equations
+                        <input type="number" value={numEquations} onChange={handleNumEquationsChange}/>
+                    </label>
+                    <br/>
+                    <label>
+                        Group Size
+                        <input type="number" value={groupSize} onChange={handleGroupSizeChange}/>
+                    </label>
+                    <br/>
+                    <label>
+                        Allow Negative Results
+                        <input
+                            type="checkbox"
+                            checked={allowNegativeResults}
+                            onChange={handleAllowNegativeResultsChange}
+                        />
+                    </label>
+                    <br/>
+                    <fieldset className="operations-group">
+                        <legend>Operations</legend>
+                        <label>
+                            <input type="checkbox" name="addition" checked={operations.addition}
+                                   onChange={handleOperationChange}/>
+                            Addition
+                        </label>
+                        <label>
+                            <input type="checkbox" name="subtraction" checked={operations.subtraction}
+                                   onChange={handleOperationChange}/>
+                            Subtraction
+                        </label>
+                        <label>
+                            <input type="checkbox" name="multiplication" checked={operations.multiplication}
+                                   onChange={handleOperationChange}/>
+                            Multiplication
+                        </label>
+                        <label>
+                            <input type="checkbox" name="division" checked={operations.division}
+                                   onChange={handleOperationChange}/>
+                            Division
+                        </label>
+                    </fieldset>
+                    <br/>
+                    <label>
+                        Print Mode:
+                        <input type="checkbox" checked={printMode} onChange={handlePrintModeChange}/>
+                    </label>
+                    <br/>
+                    {error && <div style={{color: 'red'}}>{error}</div>}
+                    <button type="submit">Start</button>
+                </form>
+            </div>
+        </div>
     );
 }
 

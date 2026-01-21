8c271d43dbba472d50272bea3e2c5a86cbfb9124
Ahmad Hajjar <ahmed_hajjar@hotmail.com>
2026-01-21 22:07:21 +0100

    Makes logo clickable and redirects home

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ function App() {
 
     return (
         <div className="App">
-            <Header/>
+            <Header onHome={handleRestart} />
             {renderStep()}
             <CookieBanner/>
         </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,23 @@ import i18n from "../i18n";
 import '../styles/Header.css'
 import logo from '../resources/logo.png'
 
-function Header() {
+function Header({ onHome }) {
     const changeLanguage = (language) => {
         i18n.changeLanguage(language).catch(err => console.error(err));
     };
 
     return (
         <header>
-            <div className="brand">
+            <div
+                className="brand"
+                onClick={onHome}
+                role="button"
+                tabIndex={0}
+                onKeyDown={(e) => {
+                    if (!onHome) return;
+                    if (e.key === 'Enter' || e.key === ' ') onHome();
+                }}
+            >
                 <img src={logo} alt="Logo"/>
             </div>
             <select value={i18n.language} onChange={(e) => changeLanguage(e.target.value)}>
